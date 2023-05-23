6cc7c6934466a3585e4d24a56c04cbbf97dc6114
markli <markasklimovas@gmail.com>
2023-05-23 15:50:51 +0300

    End of section 3

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,38 @@
+import Catalog from "../../features/catalog/Catalog";
+import { Container, CssBaseline, createTheme } from "@mui/material";
+import Header from "./Header";
+import { ThemeProvider } from "@emotion/react";
+import { useState } from "react";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme( {
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' :  '#121212'
+      }
+      
+    }
+  })
+  
+  function handleSwitch() {
+    setDarkMode(!darkMode);
+  }
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleSwitch={handleSwitch}/>
+      <Container>
+        <Catalog />
+      </Container>
+
+
+    </ThemeProvider>
+  );
+}
+
+export default App;
