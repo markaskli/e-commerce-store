8faa112589b9caaa50890c75b1b0c984fe156744
markli <markasklimovas@gmail.com>
2023-05-26 22:24:53 +0300

    End of section 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,6 +4,8 @@ import Header from "./Header";
 import { ThemeProvider } from "@emotion/react";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 
 function App() {
@@ -26,6 +28,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar theme="colored"/>
       <CssBaseline />
       <Header darkMode={darkMode} handleSwitch={handleSwitch}/>
       <Container>
