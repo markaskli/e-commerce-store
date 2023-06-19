9215cf1b668c388d7661a81ed6d0a1704327b908
markli <markasklimovas@gmail.com>
2023-06-19 21:08:23 +0300

    End of section 7

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,7 +2,7 @@ import { ShoppingCart } from "@mui/icons-material";
 import Badge from '@mui/material/Badge';
 import { AppBar, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
-import { useStoreContext } from "../context/StoreContext";
+import { useAppSelector } from "../store/configureStore";
 
 const midLinks = [
     {title: 'catalog', path: '/catalog'},
@@ -34,7 +34,7 @@ const navStyles = {
 }
 
 export default function Header({ darkMode, handleSwitch }: Props) {
-    const {basket} = useStoreContext();
+    const {basket} = useAppSelector(state => state.basket);
     const itemCount = basket?.items.reduce((sum, item) => sum += item.quantity, 0)
 
     return (
